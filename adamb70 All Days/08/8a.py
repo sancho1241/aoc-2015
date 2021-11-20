500372b87494488d39d5798026f45dca6f9e1cdf
sancho1241 <sancho12@gmx.net>
2021-11-21 00:48:22 +0100

    test

--- adamb70 All Days/08/8a.py
+++
@@ -1,9 +0,0 @@
-count = 0
-with open('8.txt', 'r') as file:
-    data = file.read()
-    for line in data.splitlines():
-        count += len(eval(line))
-
-    characters = len(data.replace('\n',''))
-
-print characters-count
