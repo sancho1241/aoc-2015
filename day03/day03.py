6a6a07d4c8800abe555c96ae525cd7e312020f34
sancho1241 <sancho12@gmx.net>
2021-12-04 00:21:17 +0100

    day03

--- day03/day03.py
+++ day03/day03.py
@@ -5,39 +5,43 @@ import string
 class SantaMap:
 
     def __init__(self):
-        self.myMap = numpy.array([0], ndmin=2)
+        self.myMap = numpy.array([0], dtype=int, ndmin=2)
         self.current_position = {
                           "x": 0,
                           "y": 0
         }
-        self.home_position = self.current_position  # set home position
+        self.home_position = self.current_position.copy()  # set home position
 
     def move(self, direction=string):
         # increase current position based on direction
         if direction == '>':
-            self.current_position["x"] += 1
+            self.current_position["y"] += 1
         elif direction == '<':
-            self.current_position["x"] -= 1
+            self.current_position["y"] -= 1
         elif direction == 'v':
-            self.current_position["y"] += 1
+            self.current_position["x"] += 1
         elif direction == '^':
-            self.current_position["y"] += 1
+            self.current_position["x"] -= 1
 
-        try:
-            self.myMap[self.current_position["x"], self.current_position["y"]] += 1
-        except Exception as e:
-            print(e)
-            if direction == '>':
-                self.myMap = numpy.append(self.myMap, 0, axis=1)
-            elif direction == '<':
-                self.myMap = numpy.insert(self.myMap, 0, 0, axis=1)
-                self.home_position["x"] += 1
-            elif direction == 'v':
-                self.myMap = numpy.append(self.myMap, 0, axis=0)
-            elif direction == '^':
-                self.myMap = numpy.insert(self.myMap, 0, 0,  axis=0)
-                self.home_position["y"] += 1
-            self.myMap[self.current_position["x"], self.current_position["y"]] += 1
+        # insert row
+        if self.current_position["x"] < 0:
+            self.myMap = numpy.insert(self.myMap, 0, 0, axis=0)
+            self.home_position["x"] += 1
+            self.current_position["x"] += 1
+        # insert column
+        elif self.current_position["y"] < 0:
+            self.myMap = numpy.insert(self.myMap, 0, 0, axis=1)
+            self.home_position["y"] += 1
+            self.current_position["y"] += 1
+        if self.current_position["x"] == self.myMap.shape[0]:
+            self.myMap = numpy.append(self.myMap, [[0] for x in self.myMap.shape[0]], axis=0)
+        elif self.current_position["y"] == self.myMap.shape[1]:
+            self.myMap = numpy.append(self.myMap, [[0] for y in range(0,self.myMap.shape[1])], axis=1)
+
+        # deliver a present to current position
+        self.myMap[self.current_position["x"], self.current_position["y"]] += 1
+
+        # deliver a present to home position
         if direction == '>':
             self.myMap[self.home_position["x"], self.home_position["y"]] += 1
 
@@ -56,4 +60,4 @@ if __name__ == "__main__":
     mySantaMap = SantaMap()
     for move in myFile:
         mySantaMap.move(move)
-    pass
+    mySantaMap.print_result1()
